2f6d3a7e8da7a5d0de52369aa9ba141ef9cca080
Thorsten Krüger <thorstenkg@gmail.com>
2017-05-31 13:00:00 +0200

    updates datastax-agent version to oldest available from datastax repo

--- attributes/datastax_agent.rb
+++ attributes/datastax_agent.rb
@@ -1,6 +1,6 @@
 # Datastax agent config options
 default['datastax-agent']['enabled'] = false
-default['datastax-agent']['version'] = '4.1.1-1'
+default['datastax-agent']['version'] = '5.0.0'
 default['datastax-agent']['conf_dir'] = '/var/lib/datastax-agent/conf'
 default['datastax-agent']['opscenter-ip'] = nil
 default['datastax-agent']['use_ssl'] = nil
